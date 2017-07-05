46dc271db08a4f1ee714e03c94ce109861bae929
Katy DiPasquale <katy.dipasquale@libertymutual.com>
2017-07-05 11:23:10 -0400

    validation working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms'; //import the forms module from @angular/forms
 
 
+
 import { AppComponent } from './app.component';
 
 @NgModule({
